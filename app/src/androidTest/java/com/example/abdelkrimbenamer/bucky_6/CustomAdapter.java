654a00bcf55cd66003918158f9638cc25938956e
karim <abdelkrimbenamer@MacBook.local>
2016-01-07 13:00:11 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/abdelkrimbenamer/bucky_6/CustomAdapter.java
@@ -0,0 +1,17 @@
+package com.example.abdelkrimbenamer.bucky_6;
+
+
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.content.Context;
+
+/**
+ * Created by abdelkrimbenamer on 16-01-06.
+ */
+public class CustomAdapter extends ArrayAdapter{
+
+    CustomAdapter(Context context, int ressource){
+
+        super (context, ressource);
+    }
+}
